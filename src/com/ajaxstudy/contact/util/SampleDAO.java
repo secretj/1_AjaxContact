8f076273389e32bef186d944688b75c1f8db83e8
secretj <qkrwlsgud890@gmail.com>
2021-09-27 11:32:30 +0900

    0927 first commit

---
+++ src/com/ajaxstudy/contact/util/SampleDAO.java
@@ -0,0 +1,25 @@
+package com.ajaxstudy.contact.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ajaxstudy.contact.domain.Contact;
+import com.ajaxstudy.contact.domain.ContactList;
+
+/*
+ 연락처 리스트를 저장하고 있는 클래스
+ DB에 연결하지 않고 컬렉션 객체에 주소데이터 보관
+ 연락처 추가, 수정, 삭제, 조회 기능 구현
+ */
+
+public class SampleDAO {
+	//연락처 목록에서 사용할 이름 목록 변수 선언
+	private static String nameList ="";
+	//연락처 목록을 담을 변수 선언
+	private static List<Contact> contacts;
+	//다음 페이지 번호를 담을 변수 선언
+	private static long naxtNo=0;
+	
+	private static 
+	
+}
